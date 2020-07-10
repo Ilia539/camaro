dbacb3b6b22eb5171e669b6fe640f4760f9600ea
Ilia539 <ilia.uliev@yandex.ru>
2020-07-10 20:07:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick__arrow slick-next"><img src="img/header/next.svg" alt="next arrow"></button>', 
+        prevArrow: '<button class="slick__arrow slick-prev"><img src="img/header/prev.svg" alt="prev arrow"></button>',
+        fade: true
+
+    
+    });
+
+});
